3b64fd07fc1d9251c8962b112fe680a75f77bd99
mdmanik74 <manikmhamud74@outlook.com>
2023-05-11 12:40:45 +0600

    number type javascript

---
+++ Number/number.js
@@ -0,0 +1,20 @@
+var number1= 0.1;
+var number2=0.6;
+
+var total= number1+number2;
+total =total.toFixed(2);
+console.log(total);
+
+var number1= 25;
+var number2=40.5;
+
+var total= +number1+ parseFloat(number2);
+
+console.log(total);
+
+var number1= 25;
+var number2=40.5;
+
+var total= +number1+ parseInt(number2);
+
+console.log(total);
