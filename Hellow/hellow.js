14967ae8d1bc130397caef8218f198147757620a
mdmanik74 <manikmhamud74@outlook.com>
2023-05-11 11:39:00 +0600

    data type javascript

--- Hellow/hellow.js
+++ Hellow/hellow.js
@@ -4,3 +4,12 @@ console.log('Good Morning');
 
 var bananaPrice =10;
 console.log(bananaPrice);
+
+var lastPrice=8;
+console.log(typeof lastPrice);
+
+var name ='Md Manik';
+console.log(typeof name);
+
+var TodayTemp=false;
+console.log(typeof TodayTemp);
