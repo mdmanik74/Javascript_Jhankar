01258c574c7585994ee8b51ed9cff76da1e56788
mdmanik74 <manikmhamud74@outlook.com>
2023-05-14 15:24:12 +0600

    javascript conditional operator

--- Conditional/conditional.js
+++ Conditional/conditional.js
@@ -24,3 +24,26 @@ if(price!=10){
 }else{
     console.log('This biscuties price high');
 }
+
+
+var ssc = true;
+var english =80;
+
+if(ssc==true && english >70){
+    console.log('This is subject pass');
+}else{
+    console.log('This is Subject fail');
+}
+
+var ssc = true;
+var english =80;
+
+if(ssc==true  || english >90){
+    console.log('This is subject pass');
+}else{
+    console.log('This is Subject fail');
+
+}
+
+ var date= new Date();
+ console.log(date);
