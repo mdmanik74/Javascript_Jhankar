ede4af579deb6ecfb8f74feb4312ffd8296c8ed1
mdmanik74 <manikmhamud74@outlook.com>
2023-05-15 12:17:58 +0600

    javascript function

---
+++ Function/Function.js
@@ -0,0 +1,30 @@
+/*
+function greet(){
+    alert('Hello World')
+}
+
+greet();
+
+function dataPass(firstName, LastName){
+    alert('hellow' + firstName+ " " + LastName);
+}
+
+greet('Bill', 'Gates');
+greet(100,200);
+
+*/
+
+function GetNumber (){
+    return 100;
+}
+let result = GetNumber();
+
+console.log(result);
+
+var add = function (num1, num2){
+    return num1+num2;
+}
+
+var result2 = add(10,20);
+
+console.log(result2);
