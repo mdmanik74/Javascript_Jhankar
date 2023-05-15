ec36e81f0cc7fcc5014a0a1747d13d72251c5e10
mdmanik74 <manikmhamud74@outlook.com>
2023-05-15 10:56:19 +0600

    data type and html alert

---
+++ Data type/datatype.js
@@ -0,0 +1,41 @@
+let myvariable = 1;  // numeric value
+
+myvariable = 'one'; // string value
+
+myvariable = true; // Boolean value
+
+
+var p2 = new Object();
+
+p2.name='Md. Manik';
+
+console.log(p2.name);
+
+var person = new Object();
+
+person.firstName;
+
+if(person.hasOwnProperty('firstName')){
+    person.firstName;
+}
+console.log(person.firstName);
+
+
+function changeFirstname(per){
+    per.NameUser='Md. Manik';
+
+}
+var personUser ={firstnames: 'bill'}
+
+
+var currentDate = new Date();
+
+console.log(currentDate);
+
+let numAry = new Array(10, 20, 30, 40, 50);
+console.log(numAry);
+
+let cities =['Mumbai', 'New Work', 'Paris', 'Sydney'];
+cities[0] = 'Delhi';
+cities[1] = 'Los Angeles';
+console.log(cities);
