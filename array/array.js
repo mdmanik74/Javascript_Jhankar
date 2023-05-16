076334f275cdf2213b70b95e060a16fae7f0978e
mdmanik74 <manikmhamud74@outlook.com>
2023-05-16 13:20:44 +0600

    array push pop

---
+++ array/array.js
@@ -0,0 +1,16 @@
+var firendsAge = [15, 17, 14, 16];
+
+var sonaliAge= firendsAge[2];
+
+firendsAge[1] = 21;
+
+var postion = firendsAge.indexOf(14);
+console.log(postion);
+
+firendsAge.push(25,28,30,32,34,36,38,40,42,44,46);
+
+console.log(firendsAge);
+
+firendsAge.pop(246);
+
+console.log(firendsAge);
